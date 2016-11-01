077307a6066e772e889181b0b2bd3d60912994fe
Stephen.Corrigan <stephen.corrigan@live.co.uk>
2016-11-01 12:32:54 -0400

    updated .py to point to heroku db

--- Donations.py
+++ Donations.py
@@ -4,10 +4,10 @@ from pymongo import MongoClient
 import json
 
 app = Flask(__name__)
-
-MONGODB_HOST = 'localhost'
-MONGODB_PORT = 27017
-DBS_NAME = 'donorsUSA'
+Mongo_URI = os.getenv('MONGODB_URI')
+MONGODB_HOST = 'ds139187.mlab.com'
+MONGODB_PORT = 39187
+DBS_NAME = 'heroku_rbcnq8cq'
 COLLECTION_NAME = 'projects'
 FIELDS = {'funding_status': True, 'school_state': True, 'resource_type': True, 'poverty_level': True,
           'date_posted': True, 'total_donations': True, 'school_county' : True, 'school_district' : True, '_id': False}
@@ -24,9 +24,10 @@ def new():
 
 @app.route("/donorsUS/projects")
 def donor_projects():
-    connection = MongoClient(MONGODB_HOST, MONGODB_PORT)
+    connection = MongoClient(MONGO_URI)
+    # connection = MongoClient(MONGODB_HOST, MONGODB_PORT)
     collection = connection[DBS_NAME][COLLECTION_NAME]
-    projects = collection.find(projection=FIELDS, limit=55000)
+    projects = collection.find(projection=FIELDS, limit=20000)
     json_projects = []
     for project in projects:
         json_projects.append(project)
